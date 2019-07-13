430457be1442ea60bc3f160e570c34872a776237
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-13 19:07:08 +0700

    Toan cuc va cục bộ

--- app/src/main/java/khoapham/ptp/phamtanphat/methodcheckvalue02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/methodcheckvalue02072019/MainActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Biến toàn cục;
+    String b ="Hello";
+    //Tạo biến ở đâu những thằng cùng cấp hoặc bé hơn
+    // thì thấy được nhau ngược lại
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Bien cục bộ
+        String a = "Hello";
+
+    }
+    public void Show(){
+       b =" 10";
     }
 }
